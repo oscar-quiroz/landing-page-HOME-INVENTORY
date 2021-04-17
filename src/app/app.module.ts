1bfecbedb875e7e0ea05ad71ef1a65c92f9adcdb
oscar-quiroz <kairox.rt@gmail.com>
2021-04-17 09:42:18 -0500

    creacion de estructura landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { ServiceComponent } from './components/service/service.component';
+import { PreferenceComponent } from './components/preference/preference.component';
+import { OpinionComponent } from './components/opinion/opinion.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HeroComponent,
+    ServiceComponent,
+    PreferenceComponent,
+    OpinionComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
